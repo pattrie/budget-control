a0b207f0d0316add03d6a48114457cf0a3c2366c
pattrie <patri.b.morais@gmail.com>
2022-02-24 14:59:51 -0300

    [feature] - swagger config

---
+++ src/main/java/com/github/pattrie/budgetcontrol/configurations/SpringFoxConfig.java
@@ -0,0 +1,31 @@
+package com.github.pattrie.budgetcontrol.configurations;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableWebMvc
+@EnableSwagger2
+@Configuration
+public class SpringFoxConfig {
+
+  @Value("${springfox.pathMapping:/}")
+  private String springFoxPathMapping;
+
+  @Bean
+  public Docket docket() {
+    return new Docket(DocumentationType.OAS_30)
+        .select()
+        .apis(RequestHandlerSelectors.basePackage("com.github.pattrie.budgetcontrol"))
+        .paths(PathSelectors.any())
+        .build()
+        .pathMapping(springFoxPathMapping);
+  }
+
+}
--- src/main/java/com/github/pattrie/budgetcontrol/controllers/RevenueController.java
+++ src/main/java/com/github/pattrie/budgetcontrol/controllers/RevenueController.java
@@ -7,6 +7,9 @@ import com.github.pattrie.budgetcontrol.controllers.jsons.RevenueRequestJson;
 import com.github.pattrie.budgetcontrol.controllers.jsons.RevenueResponseJson;
 import com.github.pattrie.budgetcontrol.domains.Revenue;
 import com.github.pattrie.budgetcontrol.usecases.RevenueService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import java.util.List;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -39,7 +42,10 @@ public class RevenueController {
 
   private final RevenueService revenueService;
 
+  @ApiOperation(value = "Create Revenue", notes = "Creates a new revenue")
+  @ApiResponses(value = {@ApiResponse(code = 200, message = "Created a new revenue successfully")})
   @PostMapping(consumes = APPLICATION_JSON_VALUE)
+  @ResponseStatus(HttpStatus.OK)
   public ResponseEntity<RevenueResponseJson> createRevenue(
       @RequestBody @Valid final RevenueRequestJson revenueRequestJson) {
     log.info("Creation of revenue :: {}", revenueRequestJson.getDescription());
@@ -47,6 +53,8 @@ public class RevenueController {
     return revenueService.create(revenue);
   }
 
+  @ApiOperation(value = "Get All Revenue", notes = "List all revenues")
+  @ApiResponses(value = {@ApiResponse(code = 200, message = "Get all revenues successfully")})
   @GetMapping(produces = APPLICATION_JSON_VALUE)
   @ResponseStatus(HttpStatus.OK)
   public List<RevenueResponseJson> getAllRevenues() {
@@ -54,35 +62,55 @@ public class RevenueController {
     return revenueService.getAll();
   }
 
+  @ApiOperation(value = "Get a Revenue by Description", notes = "Get a revenue by description")
+  @ApiResponses(
+      value = {@ApiResponse(code = 200, message = "Get a revenue by description successfully")})
   @GetMapping(value = SEARCH, produces = APPLICATION_JSON_VALUE)
   @ResponseStatus(HttpStatus.OK)
-  public List<RevenueResponseJson> getRevenueByDescription(@RequestParam("description") String description) {
+  public List<RevenueResponseJson> getRevenueByDescription(
+      @RequestParam("description") String description) {
     log.info("Find revenue by description :: {}", description);
     return revenueService.getExpenseByDescription(description);
   }
 
+  @ApiOperation(value = "Get a Revenue by ID", notes = "Get a revenue by id")
+  @ApiResponses(value = {@ApiResponse(code = 200, message = "Get a revenue by id successfully")})
   @GetMapping(value = ID, produces = APPLICATION_JSON_VALUE)
+  @ResponseStatus(HttpStatus.OK)
   public ResponseEntity<RevenueResponseJson> getRevenueBy(@PathVariable final String id) {
     log.info("Find revenue with ID :: {}", id);
     return revenueService.getBy(id);
   }
 
+  @ApiOperation(
+      value = "Get a Revenue by Year And Month",
+      notes = "Get a revenue by year and month")
+  @ApiResponses(
+      value = {@ApiResponse(code = 200, message = "Get a revenue by year and month successfully")})
   @GetMapping(value = YEAR + MONTH, produces = APPLICATION_JSON_VALUE)
+  @ResponseStatus(HttpStatus.OK)
   public ResponseEntity<List<RevenueResponseJson>> getRevenueByYearAndMonth(
       @PathVariable("year") final int year, @PathVariable("month") final int month) {
     log.info("Find revenue - Period :: {} / {}", month, year);
     return revenueService.getRevenueByYearAndMonth(month, year);
   }
 
+  @ApiOperation(value = "Update a Revenue", notes = "Update a revenue")
+  @ApiResponses(value = {@ApiResponse(code = 200, message = "Update a expense successfully")})
   @PutMapping(value = ID, consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
-  public ResponseEntity<RevenueResponseJson> updateRevenueBy(@PathVariable final String id,
+  @ResponseStatus(HttpStatus.OK)
+  public ResponseEntity<RevenueResponseJson> updateRevenueBy(
+      @PathVariable final String id,
       @RequestBody @Valid final RevenueRequestJson revenueRequestJson) {
     log.info("Update revenue with ID :: {}", id);
     final Revenue revenue = toRevenue.convert(revenueRequestJson);
     return revenueService.update(id, revenue);
   }
 
+  @ApiOperation(value = "Delete a Revenue", notes = "Delete a revenue")
+  @ApiResponses(value = {@ApiResponse(code = 200, message = "Delete a revenue successfully")})
   @DeleteMapping(value = ID)
+  @ResponseStatus(HttpStatus.OK)
   public ResponseEntity<Object> deleteRevenueBy(@PathVariable final String id) {
     log.info("Delete revenue with ID :: {}", id);
     return revenueService.delete(id);
